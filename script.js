0840ec97f89aedd9c6ba04f5ce266d4953760cfe
RodasNega <rodas.nega@ocsbstudent.ca>
2021-01-06 15:26:43 +0000

    test

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function shiftSchedule () {
     alert('Time to go to school!')
   } else if (day === 'Saturday' || day === 'Sunday') {
     alert('Time to relax for the weekend!')
-   // the conditions above will examine the user response and output an alert whether they have to go to school, work, or they have a day off depending on what they input as their age and time of day
+  // the conditions above will examine the user response and output an alert whether they have to go to school, work, or they have a day off depending on what they input as their age and time of day
   } else {
     alert('Something went wrong, try again.')
   // this else condition operates if a response was written in the text boxes that doesn't relate to the age or day or if they wrote their age and the day in the wrong text boxes
